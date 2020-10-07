d2d4683f2913a8823d0758622ee63b2a99a6d2ac
Edison Perdomo <ing.leonardo92@gmail.com>
2020-10-07 14:37:38 -0400

    Se creo el componente Card

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
-/* const element = document.createElement('h1')
-element.innerText = "Hello React"
-const container = document.getElementById('root')
-container.appendChild(element) */
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const user = {
-  firtsName : 'Raul',
-  lastName : 'Palacios',
-  avatar : ""
-}
-
-function getName(user){
-  return `${user.firtsName} ${user.lastName}`
-}
-
-function getGreeting(user){
-  if(user){
-    return <h1>Hello {getName(user)}</h1>
-  }
-
-  return <h1>Hello Stranger</h1>
-}
-
-const name = "edison"
-//const element = <div> { getGreeting(user) } </div>
-//const element = <img src={user.avatar} />
-const element = (
-  <div>
-    <h1>{getGreeting(user)}</h1>
-    <img src={user.avatar} />
-  </div>
-)
 const container = document.getElementById('root')
 
-ReactDOM.render(element,container)
+//ReactDOM.render(__QUE___,___Donde__)
+ReactDOM.render(<Card /> ,container)
 
